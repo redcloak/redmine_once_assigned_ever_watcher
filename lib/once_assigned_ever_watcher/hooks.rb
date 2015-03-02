4305bea92087985de5484286ffef61f583582fb0
spopov <redcloak2@gmail.com>
2015-03-02 14:53:31 +0300

    Update deprecated finder methods

--- lib/once_assigned_ever_watcher/hooks.rb
+++ lib/once_assigned_ever_watcher/hooks.rb
@@ -1,16 +1,12 @@
 module OnceAssignedEverWatcher
   class Hooks < Redmine::Hook::ViewListener
-  
     def controller_issues_edit_before_save (context={})
       if context[:issue] && context[:issue].id != nil                      
         issue = Issue.find_by_id(context[:issue].id)      
         if issue.assigned_to_id != nil
-          Watcher.find_or_create_by_watchable_type_and_watchable_id_and_user_id("Issue", 
-                                                                                context[:issue].id, 
-                                                                                issue.assigned_to_id)
+          Watcher.where(watchable_type: "Issue", watchable_id: context[:issue].id, user_id: issue.assigned_to_id).first_or_create
         end
       end
     end
-    
   end
 end
